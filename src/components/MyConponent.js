8db05cb2ef385ef06c9296ae85ab330c1d7209ed
TgNam <nam060720041220@gmail.com>
2024-06-10 12:36:24 +0700

    main

---
+++ src/components/MyConponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MyConponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>My frist Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+export default MyConponent;
