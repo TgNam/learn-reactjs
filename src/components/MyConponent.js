2e657b2be13822685ac2d3c1c9fe133633e2f0c7
TgNam <nam060720041220@gmail.com>
2024-06-10 17:07:52 +0700

    video 19

--- src/components/MyConponent.js
+++ src/components/MyConponent.js
@@ -6,11 +6,20 @@ class MyConponent extends React.Component {
         address: "HaiDuong",
         age: 19
     }
+    hendleClick(event){
+        console.log(">>>click me my button")
+        console.log(this.state.name)
+    }
+    hendleOnMoverOver(event){
+        console.log(event)
+    }
     //JSX
     render() {
         return (
             <div>
-                My  name is {this.state.name} and I'm from {this.state.address}. I'm {this.state.age} yeard old
+                My  name is {this.state.name} and I'm from {this.state.address}. I'm {this.state.age} yeard old\
+                <button onMouseOver={this.hendleOnMoverOver}>Click me</button>
+                <button onClick={this.hendleClick}>Click me</button>
             </div>
         );
     }
