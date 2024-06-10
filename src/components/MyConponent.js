c5023716956a66759a4e2805b6042e3198e2f55c
TgNam <nam060720041220@gmail.com>
2024-06-11 00:07:45 +0700

    video 23.2

--- src/components/MyConponent.js
+++ src/components/MyConponent.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import UserInfo from './UserInfo'
+import DisplayInfor from './DisplayInfo';
 class MyConponent extends React.Component {
-
     //JSX
     render() {
+        const myAge = 19;
+        const myInfo = ["a","b","c","d"]
         return (
             <div>
                 <UserInfo />
+                <br/><br/>
+                <DisplayInfor name='Nam' age={myAge} myInfor = {myInfo}/>
             </div>
         );
     }
