85834252ce6d5800f6e8531685d5bed1c3456a53
TgNam <nam060720041220@gmail.com>
2024-06-10 17:46:33 +0700

    video 21

--- src/components/MyConponent.js
+++ src/components/MyConponent.js
@@ -6,24 +6,27 @@ class MyConponent extends React.Component {
         address: "HaiDuong",
         age: 19
     }
-    hendleClick(event){
-        console.log(">>>click me my button")
-        console.log("My name is"+this.state.name)
+    hendleOnChange = (event) => {
         this.setState({
-            name: "TungTai",
-            age: Math.floor(Math.random() * 100+ 1)
+            name: event.target.value,
+            age: Math.floor(Math.random() * 100 + 1)
         })
     }
-    hendleOnMoverOver(event){
-        console.log(event)
+    hendleOnSubmit=(event)=>{
+        event.preventDefault();
+        console.log("My name is"+this.state.name)
     }
     //JSX
     render() {
         return (
             <div>
                 My  name is {this.state.name} and I'm from {this.state.address}. I'm {this.state.age} years old
-                <button onMouseOver={this.hendleOnMoverOver}>Click me</button>
-                <button onClick={(event)=> {this.hendleClick(event)} }>Click me</button>
+
+                <form onSubmit={(event)=>this.hendleOnSubmit(event)}>
+                    <input type='text'
+                        onChange={(event) => { this.hendleOnChange(event) }} />
+                    <button>Click me</button>
+                </form>
             </div>
         );
     }
