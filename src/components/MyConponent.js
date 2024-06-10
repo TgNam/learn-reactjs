dcb1a3934dbed5bf7cbac5d100c2b9ccd60c3609
TgNam <nam060720041220@gmail.com>
2024-06-10 13:00:59 +0700

    video 17

--- src/components/MyConponent.js
+++ src/components/MyConponent.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
 class MyConponent extends React.Component {
+    state = {
+        name: "Nam",
+        address: "HaiDuong",
+        age: 19
+    }
     //JSX
     render() {
         return (
-            <div>My frist Component
-                {Math.random()}
+            <div>
+                My  name is {this.state.name} and I'm from {this.state.address}. I'm {this.state.age} yeard old
             </div>
         );
     }
