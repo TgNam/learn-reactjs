02d67ee4290ea82e7e1b7d317dd0b0d811211f83
TgNam <nam060720041220@gmail.com>
2024-06-17 22:39:49 +0700

    video 33

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -2,8 +2,27 @@ import React from "react";
 import './DisplayInfo.scss'
 // import logo from './../logo.jpg'
 class DisplayInfor extends React.Component {
-    state = {
-        isShowListUser: true
+    constructor(props) {
+        console.log("call contructer")
+        super(props);
+        this.state = {
+            isShowListUser: true
+        }
+    }
+
+    componentDidMount() {
+        console.log("....component Did Mount ")
+        setTimeout(() => {
+            document.title = 'Nam Pro'
+        }, 3000);
+    }
+    componentDidUpdate(prevProp, prevState, snapshot) {
+        console.log("...call component did update", this.props, prevProp);
+        if (this, this.props.listArr !== prevProp.listArr) {
+            if (this.props.listArr.length === 5) {
+                alert(" you got 5 users")
+            }
+        }
     }
     hendleShowHide() {
         this.setState({
@@ -12,9 +31,10 @@ class DisplayInfor extends React.Component {
     }
 
     render() {
-        console.log(this.props)
+        console.log(">>> call me render")
+        // console.log(this.props)
         const { listArr } = this.props
-        console.table(listArr)
+        // console.table(listArr)
         return (
             <div className="display-info-conatier">
                 {/* <img src={logo} alt="Logo" /> */}
@@ -24,18 +44,18 @@ class DisplayInfor extends React.Component {
                 {this.state.isShowListUser &&
                     <>
                         {listArr.map((item) => {
-                                return (
-                                    <div key={item.id}>
-                                        <div className={item.age > 18 ? "red" : "green"}>
-                                            <div>ID: {item.id}</div>
-                                            <div>My name's {item.name}</div>
-                                            <div>I'm {item.age} years old</div>
-                                            <div>I'm from {item.address}</div>
-                                            <button onClick={() => this.props.handleDeleteUser(item.id)}>X</button>
-                                            <hr />
-                                        </div>
+                            return (
+                                <div key={item.id}>
+                                    <div className={item.age > 18 ? "red" : "green"}>
+                                        <div>ID: {item.id}</div>
+                                        <div>My name's {item.name}</div>
+                                        <div>I'm {item.age} years old</div>
+                                        <div>I'm from {item.address}</div>
+                                        <button onClick={() => this.props.handleDeleteUser(item.id)}>X</button>
+                                        <hr />
                                     </div>
-                                )
+                                </div>
+                            )
                         })}
                     </>
                 }
