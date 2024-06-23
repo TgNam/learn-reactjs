2a2e5fdece0744a552a1727d833672e57a0afa41
TgNam <nam060720041220@gmail.com>
2024-06-23 14:39:39 +0700

    video55

--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -7,81 +7,89 @@ import {
     SidebarHeader,
     SidebarFooter,
     SidebarContent,
-  } from 'react-pro-sidebar';
-  import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
-  import sidebarBg from '../../assets/bg2.jpg';
+} from 'react-pro-sidebar';
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import { DiReact } from "react-icons/di";
+import { MdDashboard } from "react-icons/md";
+import sidebarBg from '../../assets/bg2.jpg';
+import './SideBar.scss';
 const SideBar = (props) => {
     const { image, collapsed, toggled, handleToggleSidebar } = props;
     return (
         <div>
             <ProSidebar
-    //   image={image ? sidebarBg : false}
-    image={sidebarBg}
-      collapsed={collapsed}
-      toggled={toggled}
-      breakPoint="md"
-      onToggle={handleToggleSidebar}
-    >
-      <SidebarHeader>
-        <div
-          style={{
-            padding: '24px',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-            fontSize: 14,
-            letterSpacing: '1px',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-          }}
-        >
-          Nguyễn Trường Nam
-        </div>
-      </SidebarHeader>
+                //   image={image ? sidebarBg : false}
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <DiReact size={'3em'} color={"00bfff"} />
+                        Nguyễn Trường Nam
+                    </div>
+                </SidebarHeader>
 
-      <SidebarContent>
-        <Menu iconShape="circle">
-          <MenuItem
-            icon={<FaTachometerAlt />}
-            suffix={<span className="badge red">New</span>}
-          >
-            Nguyễn Trường Nam
-          </MenuItem>
-          <MenuItem icon={<FaGem />}>components</MenuItem>
-        </Menu>
-        <Menu iconShape="circle">
-          <SubMenu
-            suffix={<span className="badge yellow">3</span>}
-            icon={<FaRegLaughWink />}
-          >
-            <MenuItem>1</MenuItem>
-            <MenuItem>2</MenuItem>
-            <MenuItem>3</MenuItem>
-          </SubMenu>
-        </Menu>
-      </SidebarContent>
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        {/* <MenuItem
+                            icon={<FaTachometerAlt />}
+                            suffix={<span className="badge red">New</span>}
+                        >
+                            Nguyễn Trường Nam
+                        </MenuItem> */}
+                        <MenuItem
+                            icon={<MdDashboard />}
+                        >
+                            Dashboard
+                        </MenuItem>
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            icon={<FaGem />}
+                            title="Features"
+                        >
+                            <MenuItem>Quản lý Users</MenuItem>
+                            <MenuItem>Quản lý bài Quiz</MenuItem>
+                            <MenuItem>Quản lý câu hỏi</MenuItem>
+                        </SubMenu>
+                    </Menu>
+                </SidebarContent>
 
-      <SidebarFooter style={{ textAlign: 'center' }}>
-        <div
-          className="sidebar-btn-wrapper"
-          style={{
-            padding: '20px 24px',
-          }}
-        >
-          <a
-            href="https://github.com/azouaoui-med/react-pro-sidebar"
-            target="_blank"
-            className="sidebar-btn"
-            rel="noopener noreferrer"
-          >
-            <FaGithub />
-            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
-            viewSource
-            </span>
-          </a>
-        </div>
-      </SidebarFooter>
-    </ProSidebar>
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://github.com/azouaoui-med/react-pro-sidebar"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                viewSource
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
         </div>
     )
 }
