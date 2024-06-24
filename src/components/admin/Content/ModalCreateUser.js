3f415ebe61cb9dd584c87d775a0a0527a09db182
TgNam <nam060720041220@gmail.com>
2024-06-24 16:11:44 +0700

    video57

--- src/components/admin/Content/ModalCreateUser.js
+++ src/components/admin/Content/ModalCreateUser.js
@@ -6,12 +6,31 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './ModalCreateUser.scss'
 const ModalCreateUser = () => {
     const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
+    const handleClose = () => {
+        setShow(false)
+        console.log(email + password + username + role)
+    };
     const handleShow = () => setShow(true);
 
+    const [email, setEmail] = useState("");
+    const [password, setPasswork] = useState("");
+    const [username, setUsername] = useState("");
+    const [role, setRole] = useState("USER");
+    const [image, setImage] = useState("");
+
+    const [previewImage, setPreviewImage] = useState("")
+    const handUploadImage = (event) => {
+        if (event.target && event.target.value && event.target.files[0]) {
+            setPreviewImage(URL.createObjectURL(event.target.files[0]))
+            setImage(event.target.files[0])
+        } else {
+            setPreviewImage("")
+        }
+    }
     return (
         <>
             <Button variant="success" onClick={handleShow}>
@@ -33,13 +52,23 @@ const ModalCreateUser = () => {
                                 <Col>
                                     <Form.Group className="mb-3" controlId="formGroupEmail">
                                         <Form.Label>Email address</Form.Label>
-                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Control
+                                            type="email"
+                                            placeholder="Enter email"
+                                            value={email}
+                                            onChange={(event) => setEmail(event.target.value)}
+                                        />
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group className="mb-3" controlId="formGroupPassword">
                                         <Form.Label>Password</Form.Label>
-                                        <Form.Control type="password" placeholder="Password" />
+                                        <Form.Control
+                                            type="password"
+                                            placeholder="Password"
+                                            value={password}
+                                            onChange={(event) => setPasswork(event.target.value)}
+                                        />
                                     </Form.Group>
                                 </Col>
                             </Row>
@@ -47,23 +76,43 @@ const ModalCreateUser = () => {
                                 <Col>
                                     <Form.Group className="mb-3" controlId="formGroupName">
                                         <Form.Label>User Name</Form.Label>
-                                        <Form.Control type="text" placeholder="Name" />
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Name"
+                                            value={username}
+                                            onChange={(event) => setUsername(event.target.value)}
+                                        />
                                     </Form.Group>
                                 </Col>
                                 <Col>
                                     <Form.Group className="mb-3" controlId="formGroupRole">
                                         <Form.Label>Role</Form.Label>
-                                        <Form.Select aria-label="Default select example">
-                                            <option selected value="USER">User</option>
+                                        <Form.Select aria-label="Default select example" onChange={(event) => setRole(event.target.value)} value={role}>
+                                            <option value="USER">User</option>
                                             <option value="ADMIN">Admin</option>
                                         </Form.Select>
                                     </Form.Group>
                                 </Col>
                             </Row>
                             <Row>
-                                <div>
-                                    <Button variant="secondary"><IoIosAddCircleOutline /> Upload file image</Button>
-                                </div>
+                                <Form.Group className="mb-3" controlId="formGroupFile">
+                                    <Form.Label><IoIosAddCircleOutline /> Upload file image</Form.Label>
+                                    <Form.Control
+                                        type="file"
+                                        hidden
+                                        onChange={(event) => handUploadImage(event)}
+                                    />
+                                </Form.Group>
+                            </Row>
+                            <Row className="preview-image border border-secondary">
+                                <Col>
+                                    {
+                                        previewImage ?
+                                            <img src={previewImage} />
+                                            :
+                                            <span >Preview Image</span>
+                                    }
+                                </Col>
                             </Row>
                         </Container>
                     </Form>
